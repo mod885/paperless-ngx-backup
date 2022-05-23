0585b071a079735d2ffd4b6d4bac83cd67f5196c
mod885 <aplonne@gmail.com>
2022-05-23 19:37:15 +0200

    added combined backup and restore scripts

---
+++ backup-docker-complete.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#########################################################################
+#Name: backup-docker-compose.sh
+#https://www.laub-home.de/wiki/Docker_Backup_und_Restore_-_eine_kleine_Anleitung
+#Subscription: This Script backups the docker compose project folder
+#to a backup directory
+##by A. Laub
+#andreas[-at-]laub-home.de
+#
+#License:
+#This program is free software: you can redistribute it and/or modify it
+#under the terms of the GNU General Public License as published by the
+#Free Software Foundation, either version 3 of the License, or (at your option)
+#any later version.
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+#or FITNESS FOR A PARTICULAR PURPOSE.
+#########################################################################
+
+SCRIPT_PATH_VOLUME="/home/mod/git/paperless-ngx-backup/backup-docker-volume.sh"
+SCRIPT_PATH_COMPOSE="/home/mod/git/paperless-ngx-backup/backup-docker-compose.sh"
+
+. "$SCRIPT_PATH_VOLUME"
+. "$SCRIPT_PATH_COMPOSE"
